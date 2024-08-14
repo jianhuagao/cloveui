0547399c9414da04e7191141761cf0bcee43a6c6
jianhuagao <jianhua.gao@foxmail.com>
2024-08-14 14:24:11 +0800

    💄 首页样式

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
     <main className="relative">
       <LayoutContent>
         <div className="prose mx-auto flex max-w-7xl flex-col px-4 py-20 dark:prose-invert">
-          <h1 className="z-10 text-7xl">CloveUI</h1>
+          <h1 className="z-10 text-7xl transition-all hover:text-transparent">
+            <span className="cursor-pointer bg-gradient-to-r from-violet-500 to-pink-500 bg-clip-text">CloveUI</span>
+          </h1>
           <h1>
             Free open source <span className="cursor-pointer transition-all hover:text-[#38bdf8]">TailwindCSS</span> component
             library
--- src/components/getStarted/index.tsx
+++ src/components/getStarted/index.tsx
@@ -40,14 +40,14 @@ export default memo(function GetStarted() {
           className="size-4 w-0 transition-all group-hover:w-4"
         >
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M13.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 0 1-1.06-1.06L11.69 12 4.72 5.03a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
           <path
-            fill-rule="evenodd"
+            fillRule="evenodd"
             d="M19.28 11.47a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06L17.69 12l-6.97-6.97a.75.75 0 0 1 1.06-1.06l7.5 7.5Z"
-            clip-rule="evenodd"
+            clipRule="evenodd"
           />
         </svg>
       </button>
