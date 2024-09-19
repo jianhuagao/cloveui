b037eb1a64b4b571e93886b18521cbf02000d93d
jianhuagao <jianhua.gao@foxmail.com>
2024-09-19 18:20:34 +0800

    📝 articles

--- src/app/docs/article/directory/page.tsx
+++ src/app/docs/article/directory/page.tsx
@@ -1,14 +1,45 @@
+import { join } from 'path';
 import { memo } from 'react';
+import { promises as fs } from 'fs';
+import { serialize } from 'next-mdx-remote/serialize';
+
+interface ArticleDicProps {
+  articleId: number;
+  artTitle: string;
+  authName: string;
+  isPublished: boolean;
+  pubDate: string;
+  lastModDate: string;
+  category: string;
+}
+
+interface ArticleDicMdxProps {
+  articles: {
+    [key: string]: ArticleDicProps;
+  };
+}
 
 export default memo(async function Page() {
+  const { articles }: ArticleDicMdxProps = await getArticlesDic();
   return (
     <div className="mx-auto max-w-5xl">
       <h2>Directory</h2>
       <div className="flex flex-col gap-4 py-2">
-        <h4>1.Buildinnnnnnnng</h4>
-        <h4>2.Buildinnnnnng</h4>
-        <h4>3.Buildinnng</h4>
+        {Object.values(articles)?.map(article => {
+          return <h4 key={article.articleId}>{article.artTitle}</h4>;
+        })}
       </div>
     </div>
   );
 });
+
+async function getArticlesDic() {
+  const directoryDicPath = join(process.cwd(), '/src/data/articles/directory.mdx');
+
+  const componentItem = await fs.readFile(directoryDicPath, 'utf-8');
+
+  const mdxSource = await serialize<ArticleDicMdxProps, ArticleDicMdxProps>(componentItem, {
+    parseFrontmatter: true
+  });
+  return mdxSource.frontmatter;
+}
