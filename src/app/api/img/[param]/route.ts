e3c3b6cec4c00c4ed63d5e5d1f47c0e4823b9411
jianhuagao <jianhua.gao@foxmail.com>
2024-10-09 09:52:11 +0800

    🎨 优化

--- src/app/api/img/[param]/route.ts
+++ src/app/api/img/[param]/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 
-export async function GET(request: Request, { params }: { params: { param: string } }) {
+export async function GET(_request: Request, { params }: { params: { param: string } }) {
   const { param } = params;
 
   if (!param) {
