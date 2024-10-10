9caf5819941f9aefc8edc2a73f5dc9acc694c028
jianhuagao <jianhua.gao@foxmail.com>
2024-10-10 10:27:44 +0800

    🐛 SVG

--- src/components/getStarted/index.tsx
+++ src/components/getStarted/index.tsx
@@ -27,11 +27,11 @@ export default memo(async function GetStarted() {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           className="size-4 w-0 transition-all group-hover:w-4"
         >
-          <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          <path strokeLinecap="round" strokeLinejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
         </svg>
       </button>
     </Link>
