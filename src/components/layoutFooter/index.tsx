391980de729507b29397b32a90ccfe76d1bbacda
jianhuagao <jianhua.gao@foxmail.com>
2024-10-11 09:53:25 +0800

    💄 add email

--- src/components/layoutFooter/index.tsx
+++ src/components/layoutFooter/index.tsx
@@ -4,14 +4,12 @@ import Image from 'next/image';
 export default memo(function Footer() {
   return (
     <footer className="z-50 text-sm opacity-70">
-      <div className="mx-auto flex h-32 items-center px-20">
-        <div className="flex items-center gap-1">
-          © 2024 <Image src="/logo.svg" width={12} height={12} alt="" />
-          Clove UI
-          <a className="text-purple-400 hover:underline" href="https://github.com/jianhuagao/cloveui?tab=MIT-1-ov-file">
-            MIT License
-          </a>
-        </div>
+      <div className="mx-auto flex h-32 items-center gap-1 px-20">
+        © 2024 <Image src="/logo.svg" width={12} height={12} alt="" />
+        Clove UI
+        <a className="text-purple-400 hover:underline" href="https://github.com/jianhuagao/cloveui?tab=MIT-1-ov-file">
+          MIT License
+        </a>
         <h1 className="ml-auto opacity-80">
           Created by
           <a href="https://github.com/jianhuagao" target="_blank" className="transition-all hover:underline">
@@ -19,6 +17,12 @@ export default memo(function Footer() {
             Jianhuagao
           </a>
         </h1>
+        <a href="mailto:jianhua.gao@foxmail.com" aria-label="email" className="ml-2">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-5">
+            <path d="M1.5 8.67v8.58a3 3 0 0 0 3 3h15a3 3 0 0 0 3-3V8.67l-8.928 5.493a3 3 0 0 1-3.144 0L1.5 8.67Z" />
+            <path d="M22.5 6.908V6.75a3 3 0 0 0-3-3h-15a3 3 0 0 0-3 3v.158l9.714 5.978a1.5 1.5 0 0 0 1.572 0L22.5 6.908Z" />
+          </svg>
+        </a>
       </div>
     </footer>
   );
