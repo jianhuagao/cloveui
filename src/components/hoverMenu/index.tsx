b87fc003e6b8af3d8bb62477afd02e9d7589b0b3
jianhuagao <jianhua.gao@foxmail.com>
2024-10-12 11:33:30 +0800

    🐛 zindex

--- src/components/hoverMenu/index.tsx
+++ src/components/hoverMenu/index.tsx
@@ -29,14 +29,14 @@ export default memo(function HoverMenu({ children }: { children: React.ReactNode
       {showMenu && (
         <div
           onClick={handleCloseMenu}
-          className="not-prose absolute left-0 top-0 z-10 h-full w-full overflow-y-auto bg-[#f4f4f5] dark:bg-[#09090b]"
+          className="not-prose absolute left-0 top-0 z-40 h-full w-full overflow-y-auto bg-[#f4f4f5] dark:bg-[#09090b]"
         >
           {children}
         </div>
       )}
       <div
         onClick={() => setShowMenu(prev => !prev)}
-        className="absolute right-10 top-10 z-20 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-black md:hidden dark:bg-white"
+        className="absolute right-10 top-10 z-50 flex h-12 w-12 cursor-pointer items-center justify-center rounded-full bg-black md:hidden dark:bg-white"
       >
         <Image className="dark:invert" src="/icons/bars.svg" width={20} height={20} alt="" />
       </div>
